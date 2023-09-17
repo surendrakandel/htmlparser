1a45e8566ad03304fd9def50dff8e063752f0466
surendrakandel <20468112+surendrakandel@users.noreply.github.com>
2023-09-17 11:37:44 -0700

    updates

--- src/routes/html/+server.ts
+++ src/routes/html/+server.ts
@@ -1,17 +1,25 @@
-import { json, type RequestEvent } from "@sveltejs/kit";
+
+import puppeteer from 'puppeteer';
+import type { RequestEvent } from './$types';
 
 /**
  * 
  * @param RequestHandler
  */
 export async function GET({}:RequestEvent) {
-    let data = {
-        hello: "world"
-    }
-    return json(data,{
-        status: 200,
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto('https://google.com');
+
+    await page.setViewport({ width: 1080, height: 1024 });
+    const imgBuffer = await page.screenshot({ fullPage: true });
+
+    await browser.close();
+
+    return new Response(imgBuffer, {
         headers: {
-            "Content-Type": "application/json"
-        }
-    })
+            'Content-Type': 'image/png',
+        },
+    });
 }
